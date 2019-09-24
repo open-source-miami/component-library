14fb1c8b3f201c78fb8d857783c4729a95a38865
AndresMurguido001 <dre.murguido@gmail.com>
2019-09-24 10:38:46 -0400

    WIP: basic button. Still missing tests

---
+++ src/Button/Button.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { storiesOf } from "@storybook/react"
+
+import Button from "./index"
+
+const stories = storiesOf("Button", module)
+
+stories.add("Button", () => {
+  return <Button>Basic Button</Button>
+})
--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -7,35 +7,41 @@ import {
   compose,
   background,
   backgroundColor,
-  border,
-  borderBottom,
-  borderColor,
-  borderLeft,
-  borderTop,
-  borderRadius,
-  borderRight,
   width,
-  height
+  height,
+  space,
+  SpaceProps,
+  borders
 } from "styled-system"
 
-type Btn = ButtonStyleProps & BorderProps & LayoutProps & BackgroundProps
+type Btn = ButtonStyleProps &
+  BorderProps &
+  LayoutProps &
+  BackgroundProps &
+  SpaceProps
 
 const buttonStyled = compose(
   background,
   backgroundColor,
-  border,
-  borderBottom,
-  borderColor,
-  borderLeft,
-  borderTop,
-  borderRadius,
-  borderRight,
+  borders,
   width,
-  height
+  height,
+  space
 )
 
 const StyledButton = styled.button<Btn>`
   ${buttonStyled}
+
+  &:hover {
+    cursor: pointer;
+  }
 `
+
+StyledButton.defaultProps = {
+  background: "#fff",
+  border: "1px solid #777",
+  borderRadius: "5px",
+  padding: "0.5rem"
+}
 
 export default StyledButton
